46f67185c137c7c2d67f8d00d2756b855309fcf1
isen-sama <arnakorn567@gmail.com>
2024-06-30 21:24:18 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+'''
+download library pyInstaller 
+open terminal >> pip install pyinstaller
+'''
+
+import webbrowser 
+webbrowser.open("https://youtu.be/dCEMSaho0io?si=mdLeoulAYkQro3bR")
+
+'''
+build .exe file
+open terminal >> pyinstaller --onefile test.py or python -m PyInstaller --onefile test.py
+'''
